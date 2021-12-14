34ad5b2cffb652794ed2058bbe6d811804e7c62c
Olivier <o.ettlin@gmail.com>
2021-12-14 21:31:49 +0100

    Removing test files

--- templates/upload.php
+++ templates/upload.php
@@ -1,4 +1,4 @@
-<form method="POST" action="<?= ROOT_URL ?>/controller/result.php">
+<form method="POST" action="../controller/result.php">
     <label for="inpData">Please paste your data: </label>
     <input type="textarea" name="inpData" id="inpData" rows="10">
 </form>
